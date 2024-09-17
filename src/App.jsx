21eac4bd3531dfd56b5d00cb99fbbfea8c7e4c07
Victor Mwadime <victormwadime@gmail.com>
2024-09-17 14:19:13 +0300

    Add initial setup and data fetching from Giphy API and basic rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Memory Card Game</h1>;
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.giphy.com/v1/gifs/trending?api_key=dWOvpluH6YNYSVI1sX7BHZfuq64sVBWF&limit=12"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
