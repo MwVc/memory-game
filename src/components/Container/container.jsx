20d8ee038fe5ced81d26b3eb658c573de4facc70
Victor Mwadime <victormwadime@gmail.com>
2024-09-18 17:23:49 +0300

    Create a shuffle function to shuffle cards

--- src/components/Container/container.jsx
+++ src/components/Container/container.jsx
@@ -1,17 +1,23 @@
-import PropTypes from "prop-types";
+import PropTypes, { func } from "prop-types";
 import Card from "./Card/Card";
 
 export default function Container(props) {
-  console.log(props.data[11]);
-  return (
-    <div className="container">
-      {props.data.map((value, index) => (
-        <Card value={value} key={index} />
-      ))}
-    </div>
-  );
+  const cardList = props.data.map((value, index) => (
+    <Card value={value} key={index} />
+  ));
+  console.log(Array.isArray(cardList));
+  return <div className="container">{cardList}</div>;
 }
 
 Container.propTypes = {
   data: PropTypes.array,
 };
+
+function shuffleCards(cards) {
+  let shuffledCards = cards
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
+  return shuffledCards;
+}
