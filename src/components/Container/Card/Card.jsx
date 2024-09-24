7b0bfbf8bbc09d39f25c9613e8c1b41d35a1a198
Victor Mwadime <victormwadime@gmail.com>
2024-09-24 09:01:19 +0300

    Add click handling and conditional styling to Card component

--- src/components/Container/Card/Card.jsx
+++ src/components/Container/Card/Card.jsx
@@ -1,13 +1,25 @@
 import PropTypes from "prop-types";
 
-export default function Card({ value }) {
+export default function Card({ value, handleClick }) {
   return (
-    <div className="card">
-      <h1>{value}</h1>
+    <div
+      className="card"
+      onClick={handleClick}
+      style={{
+        backgroundColor: value.isClicked ? "red" : "white",
+        border: "1px red solid",
+        borderRadius: "5px",
+      }}
+    >
+      <h1>{value.value}</h1>
     </div>
   );
 }
 
 Card.propTypes = {
-  value: PropTypes.number,
+  value: PropTypes.shape({
+    value: PropTypes.any.isRequired,
+    isClicked: PropTypes.bool.isRequired,
+  }).isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
