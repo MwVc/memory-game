b0767ff1d7390cbf146c0a03cfdc8285f118865e
Victor Mwadime <victormwadime@gmail.com>
2024-09-24 08:59:18 +0300

    Add click handling and data shuffling to Container component

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,15 +1,27 @@
 import PropTypes from "prop-types";
 import Card from "./Card/Card";
+import { shuffleArray } from "../../utilities";
 
 export default function Container({ data, setData }) {
+  function handleClick(value) {
+    if (!value.isClicked) {
+      const updatedData = data.map((item) =>
+        item === value ? { ...item, isClicked: true } : item
+      );
+      setData(shuffleArray(updatedData));
+    } else {
+      console.log("This card has already been clicked. No reshuffle.");
+    }
+  }
+
   const cardList = data.map((value, index) => (
-    <Card value={value} key={index} data={data} setData={setData} />
+    <Card value={value} key={index} handleClick={() => handleClick(value)} />
   ));
-  console.log(Array.isArray(cardList));
+
   return <div className="container">{cardList}</div>;
 }
 
 Container.propTypes = {
-  data: PropTypes.array,
-  setData: PropTypes.func,
+  data: PropTypes.array.isRequired,
+  setData: PropTypes.func.isRequired,
 };
