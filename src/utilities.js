2618f967eac583b9c1fb54e45ec1447e403945e7
Victor Mwadime <victormwadime@gmail.com>
2024-09-25 11:21:24 +0300

    Delete fetchNumbers function that simulated mock API call

--- src/utilities.js
+++ src/utilities.js
@@ -1,28 +1,3 @@
-export function fetchNumbers() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const shouldFail = Math.random() > 0.8;
-      if (shouldFail) {
-        reject("Failed to fetch numbers. Server error!");
-      } else {
-        // Create an array of numbers first
-        const numbers = Array.from({ length: 12 }, () =>
-          Math.floor(Math.random() * 100)
-        );
-
-        // Convert numbers into an array of objects with same structure
-        const data = numbers.map((number) => ({
-          id: number, // for example, assign the number as 'id'
-          value: number, // use the same number for 'value'
-        }));
-
-        // Resolve the data as an array of objects
-        resolve({ json: () => Promise.resolve(data) });
-      }
-    }, 500);
-  });
-}
-
 export function shuffleArray(array) {
   let shuffledArray = array
     .map((value) => ({ value, sort: Math.random() }))
