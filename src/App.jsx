0e8cd2e34dea09979e2dc64b8c379a831d71c700
Victor Mwadime <victormwadime@gmail.com>
2024-10-16 12:33:24 +0300

    restructure App component and add bootstrap utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -31,31 +31,35 @@ function App() {
   }, []);
 
   return (
-    <div className="app">
-      <nav className="nav">
-        <h1>Memory Card Game</h1>
-        <h3>High Score: {highScore}</h3>
-        <h3>Score: {score}</h3>
+    <div className="App d-flex flex-column justify-content-between">
+      <nav className="bg-dark text-white">
+        <div className="container d-flex flex-column align-items-center">
+          <h1>Memory Game</h1>
+          <div>
+            <h3>Current Score: {score}</h3>
+            <h3>High Score: {highScore}</h3>
+          </div>
+        </div>
       </nav>
-      <div className="game">
-        <Container
-          data={data}
-          setData={setData}
-          score={score}
-          setScore={setScore}
-          highScore={highScore}
-          setHighScore={setHighScore}
-        />
-        <div className="overview">
-          <h3>Overview</h3>
-          <p>
+      <Container
+        data={data}
+        setData={setData}
+        score={score}
+        setScore={setScore}
+        highScore={highScore}
+        setHighScore={setHighScore}
+      />
+      <footer className="bg-dark text-white p-4">
+        <div className="container d-flex flex-column align-items-center">
+          <h2>Overview</h2>
+          <h5>
             The goal of the game is to click on a card without selecting the
             same card twice. Each time a card is clicked, the deck is shuffled.
             If a card is clicked twice, the game resets the score. The game also
             keeps track of the highest score achieved in the current session.
-          </p>
+          </h5>
         </div>
-      </div>
+      </footer>
     </div>
   );
 }
